393fa72a2d96f83b70db97c858d6a8aef8774bf3
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-08-22 00:03:38 +0300

    added users controller

---
+++ dist/users/users.controller.d.ts
@@ -0,0 +1,5 @@
+export declare class UsersController {
+    getUsers(query: any): string;
+    getUserById(params: any): string;
+    createUser(body: any): string;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers(@Query() query: any) {
+    return `Users with query: ${JSON.stringify(query)}`;
+  }
+
+  @Get('/:userId/:optional?')
+  getUserById(@Param() params: any) {
+    return `User with params: ${JSON.stringify(params)}`;
+  }
+
+  @Post()
+  createUser(@Body() body: any) {
+    return `Creating user with body: ${JSON.stringify(body)}`;
+  }
+}
