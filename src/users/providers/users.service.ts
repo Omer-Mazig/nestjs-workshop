226fab649fb4984a4347120b7ed06c1f3c1bda6f
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-08-29 22:37:32 +0300

    added compodocs and js docs comment for user service

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -2,13 +2,22 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { AuthService } from 'src/auth/providers/auth.service';
 import { GetUserParamsDto } from '../dtos/get-users-params.dto';
 
+/**
+ * Class to conect to users table and perform business operations
+ */
 @Injectable()
 export class UsersService {
+  /**
+   * A constructor for inject deps
+   */
   constructor(
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
 
+  /**
+   * The method to get all the users for the database
+   */
   public findAll(
     getUserParamsDto: GetUserParamsDto,
     limit: number,
@@ -29,6 +38,9 @@ export class UsersService {
     ];
   }
 
+  /**
+   * Finding a single User by id
+   */
   public findOneById(id: string) {
     return {
       id: 1234,
