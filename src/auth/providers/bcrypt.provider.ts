468dca05828c1d69ea42b9a5c8773931355c2e36
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-02 17:04:11 +0300

    aded implemntation for bcrtying

--- src/auth/providers/bcrypt.provider.ts
+++ src/auth/providers/bcrypt.provider.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { HashingProvider } from './hashing.provider';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class BcryptProvider implements HashingProvider {
-  hashPassword(data: string | Buffer): Promise<string> {
-    throw new Error('Method not implemented!');
+  public async hashPassword(data: string | Buffer): Promise<string> {
+    const salt = await bcrypt.genSalt();
+    return bcrypt.hash(data, salt);
   }
+
   comparePassword(data: string | Buffer, encrypted: string): Promise<boolean> {
-    throw new Error('Method not implemented!');
+    return bcrypt.compare(data, encrypted);
   }
 }
