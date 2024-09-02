e93c6bd29408e3e821a40bf557a95230f7587d82
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-02 12:34:39 +0300

    calculating pages logic

--- src/common/pagination/providers/pagination.provider.ts
+++ src/common/pagination/providers/pagination.provider.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PaginationQueryDto } from '../dtos/pagination-query.dto';
 import { ObjectLiteral, Repository } from 'typeorm';
+import { Request } from 'express';
+import { REQUEST } from '@nestjs/core';
 
 @Injectable()
 export class PaginationProvider {
+  constructor(
+    @Inject(REQUEST)
+    private readonly request: Request,
+  ) {}
+
   public async paginateQuery<T extends ObjectLiteral>(
     paginationQuery: PaginationQueryDto,
     repository: Repository<T>,
@@ -12,6 +19,25 @@ export class PaginationProvider {
       skip: (paginationQuery.page - 1) * paginationQuery.limit,
       take: paginationQuery.limit,
     });
+
+    const baseURL =
+      this.request.protocol + '://' + this.request.headers.host + '/';
+    const newUrl = new URL(this.request.url, baseURL);
+
+    console.log(newUrl);
+
+    const totalItems = await repository.count();
+    const totalPages = Math.ceil(totalItems / paginationQuery.limit);
+
+    const nextPage =
+      paginationQuery.page === totalPages
+        ? paginationQuery.page
+        : paginationQuery.page + 1;
+
+    const previosPage =
+      paginationQuery.page === 1
+        ? paginationQuery.page
+        : paginationQuery.page - 1;
 
     return results;
   }
