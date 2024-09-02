c64bf3be4d3f3aae7e55ee1b2d1eb7e16f31be88
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-02 19:22:23 +0300

    added jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,19 +5,35 @@ import { HashingProvider } from './providers/hashing.provider';
 import { BcryptProvider } from './providers/bcrypt.provider';
 import { UsersModule } from 'src/users/users.module';
 import { SignInProvider } from './providers/sign-in.provider';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from './config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [
     AuthService,
-    // Because HashingProvider is abstract class
+    // The HashingProvider is an abstract class, so we need to specify
+    // which concrete class should be used when it is inject
     {
       provide: HashingProvider,
       useClass: BcryptProvider,
     },
     SignInProvider,
   ],
-  imports: [forwardRef(() => UsersModule)],
+  imports: [
+    // Use forwardRef to avoid circular dependency issues between
+    // AuthModule and UsersModule.
+    forwardRef(() => UsersModule),
+
+    // Register the JWT configuration, making it available via the ConfigService
+    ConfigModule.forFeature(jwtConfig),
+
+    // Dynamically register the JWT module using the configuration provided
+    // by the jwtConfig. This allows us to configure JWT options like secret
+    // and expiration based on environment variables.
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+  ],
   exports: [AuthService, HashingProvider],
 })
 export class AuthModule {}
