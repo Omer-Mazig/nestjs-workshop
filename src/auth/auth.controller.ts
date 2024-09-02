2c1c14eece4d8dd0be1585f57bee12fa59ea0ae0
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-02 18:49:15 +0300

    fix http code to 200

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { SignInDto } from './dtos/signin.dto';
 import { AuthService } from './providers/auth.service';
 
@@ -12,6 +12,7 @@ export class AuthController {
   ) {}
 
   @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
   public async signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
