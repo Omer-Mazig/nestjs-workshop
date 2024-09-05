10a175d39040209950f35d6cca8985fc8cb5c523
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-05 16:19:08 +0300

    added endpoint to auth controller

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -2,6 +2,8 @@ import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { SignInDto } from '../dtos/signin.dto';
 import { UsersService } from 'src/users/providers/users.service';
 import { SignInProvider } from './sign-in.provider';
+import { RefreshTokenDto } from '../dtos/refresh-token.dto';
+import { RefreshTokensProvider } from './refresh-tokens.provider';
 
 @Injectable()
 export class AuthService {
@@ -16,9 +18,17 @@ export class AuthService {
      * Injecting SignInProvider
      */
     private readonly signInProvider: SignInProvider,
+    /**
+     * Injecting RefreshTokensProvder
+     */
+    private readonly refreshTokensProvder: RefreshTokensProvider,
   ) {}
 
   public async signIn(signInDto: SignInDto) {
     return await this.signInProvider.signIn(signInDto);
+  }
+
+  public async refreshTokens(refreshTokenDto: RefreshTokenDto) {
+    return await this.refreshTokensProvder.refreshTokens(refreshTokenDto);
   }
 }
