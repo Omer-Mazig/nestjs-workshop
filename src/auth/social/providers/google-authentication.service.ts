4d45b28bac3364395c4b2650cc7a24cf942de3a8
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-07 10:47:58 +0300

    added googleauth service

--- src/auth/social/providers/google-authentication.service.ts
+++ src/auth/social/providers/google-authentication.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { OAuth2Client } from 'google-auth-library';
+import jwtConfig from 'src/auth/config/jwt.config';
 
 @Injectable()
-export class GoogleAuthenticationService {}
+export class GoogleAuthenticationService implements OnModuleInit {
+  private oauthClien: OAuth2Client;
+
+  constructor(
+    /**
+     * Injecting jwtConfiguration
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  onModuleInit() {
+    const clientId = this.jwtConfiguration.googleClientId;
+    const clientSercret = this.jwtConfiguration.googleClientSecret;
+    this.oauthClien = new OAuth2Client(clientId, clientSercret);
+  }
+}
