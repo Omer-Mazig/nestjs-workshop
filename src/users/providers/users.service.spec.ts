f2cf58d657cc1fc298e7ed931093c65904394502
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-13 14:10:36 +0300

    fix spec for user service

--- src/users/providers/users.service.spec.ts
+++ src/users/providers/users.service.spec.ts
@@ -1,40 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
-import { UsersCreateManyProvider } from './users-create-many.provider';
+
+import { CreateGoogleUserProvider } from './create-google-user.provider';
+import { CreateUserDto } from '../dtos/create-user.dto';
+import { CreateUserProvider } from './create-user.provider';
 import { DataSource } from 'typeorm';
-import { getRepositoryToken } from '@nestjs/typeorm';
+import { FindOneByGoogleIdProvider } from './find-one-by-google-id.provider';
+import { FindOneUserByEmailProvider } from './find-one-user-by-email.provider';
 import { User } from '../user.entity';
-import { MailService } from 'src/mail/providers/mail.service';
-import { HashingProvider } from 'src/auth/providers/hashing.provider';
-import { CreateManyUsersDto } from '../dtos/create-many-users.dto';
+import { UsersCreateManyProvider } from './users-create-many.provider';
+import { UsersService } from './users.service';
+import { create } from 'domain';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
-    const mockUsersCreateManyProvider: Partial<UsersCreateManyProvider> = {};
+    const mockCreateUsersProvider: Partial<CreateUserProvider> = {
+      createUser: (createUserDto: CreateUserDto) =>
+        Promise.resolve({
+          id: 12,
+          firstName: createUserDto.firstName,
+          lastName: createUserDto.lastName,
+          email: createUserDto.email,
+          password: createUserDto.password,
+        }),
+    };
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UsersService,
-        {
-          provide: UsersCreateManyProvider,
-          useValue: mockUsersCreateManyProvider,
-        },
-
         { provide: DataSource, useValue: {} },
         { provide: getRepositoryToken(User), useValue: {} },
-
-        { provide: MailService, useValue: {} },
-
-        { provide: HashingProvider, useValue: {} },
+        { provide: CreateUserProvider, useValue: mockCreateUsersProvider },
+        { provide: UsersCreateManyProvider, useValue: {} },
+        { provide: FindOneUserByEmailProvider, useValue: {} },
+        { provide: FindOneByGoogleIdProvider, useValue: {} },
+        { provide: CreateGoogleUserProvider, useValue: {} },
       ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be define', () => {
+  it('Should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('Should be defined', () => {
+      expect(service.createUser).toBeDefined();
+    });
+
+    it('Should call createUser on createUserProvider', async () => {
+      let user = await service.createUser({
+        firstName: 'john',
+        lastName: 'Doe',
+        email: 'john@doe.com',
+        password: 'password',
+      });
+      expect(user.firstName).toEqual('john');
+    });
   });
 });
