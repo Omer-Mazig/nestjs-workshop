c7495a513dfe0ac450698d877ea145537be66ac6
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-13 13:26:56 +0300

    added spec to user service

--- src/users/providers/users.service.spec.ts
+++ src/users/providers/users.service.spec.ts
@@ -1,16 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { UsersCreateManyProvider } from './users-create-many.provider';
+import { DataSource } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../user.entity';
+import { MailService } from 'src/mail/providers/mail.service';
+import { HashingProvider } from 'src/auth/providers/hashing.provider';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: UsersCreateManyProvider, useValue: {} },
+
+        { provide: DataSource, useValue: {} },
+        { provide: getRepositoryToken(User), useValue: {} },
+
+        { provide: MailService, useValue: {} },
+
+        { provide: HashingProvider, useValue: {} },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('Controller should be define', () => {});
+  it('should be define', () => {
+    expect(service).toBeDefined();
+  });
 });
