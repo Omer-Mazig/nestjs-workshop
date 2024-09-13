23031471a7c2bacbb5cd90c318894b0e73d4571c
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-13 18:51:39 +0300

    added e2e spec for user.post

--- test/users/users.post.e2e-spec.ts
+++ test/users/users.post.e2e-spec.ts
@@ -1,15 +1,30 @@
-import { INestApplication } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { dropDatabase } from 'test/helpers/drop-database.helper';
-import { bootstrapNestApplication } from 'test/helpers/bootstrap-nest-application.helper';
+import * as request from 'supertest';
 
-describe('[Users] @Post Endpoint', () => {
+import { App } from 'supertest/types';
+import { ConfigService } from '@nestjs/config';
+import { INestApplication } from '@nestjs/common';
+import { bootstrapNestApplication } from 'test/helpers/bootstrap-nest-application.helper';
+import { dropDatabase } from 'test/helpers/drop-database.helper';
+import {
+  completeUser,
+  missingEmail,
+  missingFirstName,
+  missingPassword,
+} from './users.post.e2e-spec.sample-data';
+import { User } from 'src/users/user.entity';
+
+describe('[Users] @Post Endpoints', () => {
   let app: INestApplication;
   let config: ConfigService;
+  let httpServer: App;
 
   beforeEach(async () => {
+    // Instantiate the app
     app = await bootstrapNestApplication();
+    // Get the config
     config = app.get<ConfigService>(ConfigService);
+    // get server endpoint
+    httpServer = app.getHttpServer();
   });
 
   afterEach(async () => {
@@ -17,11 +32,30 @@ describe('[Users] @Post Endpoint', () => {
     await app.close();
   });
 
-  it.todo('/users - Endpoint is public');
-  it.todo('/users - firstName is mandatory');
-  it.todo('/users - lastName is mandatory');
-  it.todo('/users - email is mandatory');
-  it.todo('/users - Valid request successfully creates user');
+  it('/users - Endpoint is public', () => {
+    return request(httpServer).post('/users').send({}).expect(400);
+  });
+
+  it('/users - firstName is mandatory', () => {
+    return request(httpServer)
+      .post('/users')
+      .send(missingFirstName)
+      .expect(400);
+  });
+
+  it('/users - email is mandatory', () => {
+    return request(httpServer).post('/users').send(missingEmail).expect(400);
+  });
+
+  it('/users - password is mandatory', () => {
+    return request(httpServer).post('/users').send(missingPassword).expect(400);
+  });
+
   it.todo('/users - password is not returned in response');
+
   it.todo('/users - googleId is not returned in response');
+
+  // it('/users - Valid request successfully creates user', () => {
+  //   return request(httpServer).post('/users').send(completeUser).expect(User);
+  // });
 });
