1bca91071633b17f88e8290ecb67034f083de84f
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-13 16:01:27 +0300

    added app create function to use middlewares

---
+++ src/app.create.ts
@@ -0,0 +1,47 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
+
+export function appCreate(app: INestApplication): void {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Strips out any properties that are not part of the DTO
+      whitelist: true,
+
+      // Throws an error if any properties that are not part of the DTO are present in the request
+      forbidNonWhitelisted: true,
+
+      // Automatically transforms input data types to match the types defined in the DTO
+      transform: true,
+
+      transformOptions: {
+        // Enables implicit type conversion, so strings can be automatically converted to numbers, booleans, etc.
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('My Blog App API')
+    .setDescription('Use the base API URL as http://localhost:3000')
+    .setTermsOfService('http://localhost:3000/term-of-service')
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
+  // get the config service from the app module
+  const configService = app.get(ConfigService);
+  config.update({
+    credentials: {
+      accessKeyId: configService.get('appConfig.awsAccesskeyId'),
+      secretAccessKey: configService.get('appConfig.awsSecretAccessKey'),
+    },
+    region: configService.get('appConfig.awsRegion'),
+  });
+
+  app.enableCors();
+}
