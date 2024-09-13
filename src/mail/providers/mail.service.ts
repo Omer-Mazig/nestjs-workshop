a5ebdfdc34df9f9f836204b090ef08e969b15c47
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-13 09:14:34 +0300

    added send user Welcome method

--- src/mail/providers/mail.service.ts
+++ src/mail/providers/mail.service.ts
@@ -1,4 +1,24 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  constructor(private mailerService: MailerService) {}
+
+  public async sendUserWelcome(user: User): Promise<void> {
+    await this.mailerService.sendMail({
+      to: user.email,
+      from: `Onboarding Team <support@nestjs-blog.com>`,
+      subject: 'Welcome to NestJs blog',
+      template: './welcome',
+
+      // add any proprty you want. this will be available in ejs file
+      context: {
+        name: user.firstName,
+        email: user.email,
+        loginUrl: 'http://localhost:3000',
+      },
+    });
+  }
+}
