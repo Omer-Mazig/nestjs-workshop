a316e1b1caafff96e62d53b642a35dfd1c7d5c63
Omer-Mazig <114055368+Omer-Mazig@users.noreply.github.com>
2024-09-13 16:32:18 +0300

    ADDED POST ANDPOINT TEST FOR USERS

---
+++ test/helpers/drop-database.helper.ts
@@ -0,0 +1,21 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+
+export async function dropDatabase(config: ConfigService): Promise<void> {
+  // Create connection
+  const AppDataSource = await new DataSource({
+    type: 'postgres',
+    synchronize: config.get('database.synchronize'),
+    port: +config.get('database.port'),
+    username: config.get('database.username'),
+    password: config.get('database.password'),
+    host: config.get('database.host'),
+    database: config.get('database.name'),
+  });
+
+  // Drop all tables
+  await AppDataSource.dropDatabase();
+
+  // Close connection
+  await AppDataSource.destroy();
+}
--- test/users/users.post.e2e-spec.ts
+++ test/users/users.post.e2e-spec.ts
@@ -1,20 +1,37 @@
 import { INestApplication } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from 'src/app.module';
+import { appCreate } from 'src/app.create';
+import { dropDatabase } from 'test/helpers/drop-database.helper';
 
 describe('[Users] @Post Endpoint', () => {
   let app: INestApplication;
+  let config: ConfigService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, ConfigModule],
+      providers: [ConfigService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    config = app.get<ConfigService>(ConfigService);
+    appCreate(app);
+
     await app.init();
   });
 
   afterEach(async () => {
+    await dropDatabase(config);
     await app.close();
   });
+
+  it.todo('/users - Endpoint is public');
+  it.todo('/users - firstName is mandatory');
+  it.todo('/users - lastName is mandatory');
+  it.todo('/users - email is mandatory');
+  it.todo('/users - Valid request successfully creates user');
+  it.todo('/users - password is not returned in response');
+  it.todo('/users - googleId is not returned in response');
 });
